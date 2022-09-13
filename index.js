075fc5cd45ae255932dddd096dd7a3523bb8e2dd
Iulia-Andreea20 <griandreea4@gmail.com>
2022-09-13 04:40:09 +0300

    html structure and bubble implementation

---
+++ index.js
@@ -0,0 +1,8 @@
+let donationAmount = document.querySelector('#donation-amount');
+let donateBtn = document.querySelector('#donate-btn');
+let amountNeeded = 500;
+
+
+donateBtn.addEventListener('click', function () {
+    console.log("ok")
+});
