85b21f57a6236e5f89c6b6a35b7fe77c25a0a1c9
Iulia-Andreea20 <griandreea4@gmail.com>
2022-09-15 22:31:37 +0300

    add interactive bar

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-let donationAmount = document.querySelector('#donation-amount');
-let donateBtn = document.querySelector('#donate-btn');
-let amountNeeded = 500;
+$(document).ready(function () {
+    let donationAmount = document.querySelector('#donation-amount');
+    let donateBtn = document.querySelector('#donate-btn');
+    let amountNeeded = 500;
+    let accumulatedMoney = 0;
 
+    $('.money-needed').text('$' + amountNeeded + ' still needed for project');
 
-donateBtn.addEventListener('click', function () {
-    console.log("ok")
+    donationAmount.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {
+            let progress = $('.progress-bar .visual-progress')
+            let counter_value = 0;
+            let moneyNeededAfterDonation = amountNeeded - Number(donationAmount.value);
+            if (Number(donationAmount.value) >= 100) {
+                counter_value = (Number(donationAmount.value) * 100) / amountNeeded;
+            }
+            else {
+                counter_value = Number(donationAmount.value);
+            }
+
+            if (counter_value >= 0 && counter_value <= amountNeeded) {
+
+                $('.money-needed').text('$' + moneyNeededAfterDonation + ' still needed for project');
+                progress.css({ 'width': counter_value + '%' });
+            }
+
+        }
+
+    });
+
+    donateBtn.addEventListener("click", function () {
+        if (localStorage.getItem("accumulatedMoney") !== null) {
+            localStorage.setItem("accumulatedMoney", Number(JSON.parse(localStorage.getItem("accumulatedMoney")) + Number(donationAmount.value)));
+            console.log(localStorage.getItem("accumulatedMoney"));
+        }
+        else {
+            localStorage.setItem("accumulatedMoney", JSON.stringify(Number(donationAmount.value)));
+            console.log(localStorage.getItem("accumulatedMoney"));
+        }
+
+    });
+
 });
+
